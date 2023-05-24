6980cadac607da74bbef46ed482c4897e662bb9b
AleMB2002 <alejandramasquez@hotmail.com>
2023-05-24 15:00:16 -0300

    java

--- README.md
+++ README.md
@@ -8,5 +8,6 @@ Cree el encabezado de la pagina agregando el logo y tambien usando ":hover". Ter
 ** 8/5 **
 Empece con la idea de la segunda pagina dentro de la misma, el tema sería el cuidado de las uñas y del diseño sobre ella. 
 
-
+** 21/5 **
+Arme la estructura del contenido principal, también de la segunda página donde voy a poner los cursos, me falta ponerle estilos y acomodarlos. También quiero que la segunda página tenga un menu desplegable donde voy a poner fotos y videos de los procedimientos. 
 
---
+++ scripts/script.js
@@ -0,0 +1,46 @@
+const slider = document.querySelector ("#slider");
+let sliderSection = document.querySelectorAll(".slider__section");
+let sliderSectionLast = sliderSection [sliderSection.leght -1];
+
+const btnLeft = document.querySelector ("#btn-left");
+const btnRight= document.querySelector ("#btn-right");
+
+slider.insertAdjacentElement('afterbegin', sliderSectionLast);
+
+function moverDerecha() {
+    let sliderSectionFirst = document.querySelectorAll (".slider__section")[0];
+    slider.style.margingLeft = "-200%";
+    slider.style.transition = "all 0.5s";
+    setTimeout(function() {
+        slider.style.transition = "none";
+        slider.insertAdjacentElement('beforeend', sliderSectionFirst);
+        slider.style.margingLeft = "-100%";
+    }, 500);
+}
+
+function moverIzquierda() {
+    let sliderSection = document.querySelectorAll (".slider__section");
+    let sliderSectionLast = sliderSection [sliderSection.lenght -1];
+    slider.style.margingLeft = "0";
+    slider.style.transition = "all 0.5s";
+    setTimeout(function() {
+        slider.style.transition = "none";
+        slider.insertAdjacentElement('afterbegin', sliderSectionLast);
+        slider.style.margingLeft = "-100%";
+    }, 500);
+}
+
+btnRight.addEventListener('click', function(){
+    moverDerecha();
+})
+
+btnLeft.addEventListener('click', function(){
+    moverIzquierda();
+})
+
+
+
+/* querySelector: Devuelve el primer elemento del documento (utilizando un recorrido primero en profundidad pre ordenado de los nodos del documento) que coincida con el grupo especificado de selectores, aca se usa como un selector de css 
+sliderSection:cada una de las variables lo modifico ahi
+sliderSection.leght -1: con esto obtengo siempre la ultima imagen
+insertAdjacentElement: obtener el ultimo elemento y colocarlo en el slider, en el orde que quiera*/
